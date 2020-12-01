f251f1da1fb2bf808a5b813be51bda71c94774b0
alserg80 <asn80.1@gmail.com>
2020-12-01 03:01:30 +0300

    Complete task 2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,19 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+
         System.out.println(bean.getMessage());
+        System.out.println(catBean1.getMessage());
+
+        System.out.println("Beans \"HelloWorld\" are equals: " + (bean == bean1));
+        System.out.println("Beans \"Cat\" are equals: " + (catBean1 == catBean2));
     }
 }
